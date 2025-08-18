cc2ac701fafc7cf4ea7424fb86560c87fde2693c
Avinash Pandey <manikant7262@gmail.com>
2025-08-19 02:52:43 +0530

    Updated Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -48,7 +48,7 @@ export default function Header() {
                 Admin
               </Link>
             )}
-            {isSignedIn ? (
+            {isSignedIn && (
               <UserButton afterSignOutUrl="/" />
             )}
           </nav>
